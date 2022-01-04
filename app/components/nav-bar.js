dc95770be7ee83c9f547a440bac9d0138d3269cb
ahristian <hristian.andrei@gmail.com>
2022-01-03 20:48:18 -0700

    first commit

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,4 +1,19 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class NavBarComponent extends Component {
+  @tracked isShow = this.args.isShow ?? null;
+
+  @action toggle() {
+    if (this.isShow === true) {
+      this.isShow = null;
+    } else {
+      this.isShow = true;
+    }
+  }
+
+  @action someAction(){
+    this.isShow = null;
+  }
 }
